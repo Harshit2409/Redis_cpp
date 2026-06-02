5c39c562efab3392d64ed4f6bd5815bfab859003
Harshit Tiwari <harshit.tiwari@gentrack.com>
2026-06-02 22:37:13 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,7 @@ std::vector<char*> parserRESP(char *buffer)
   {
     token = std::strtok(nullptr, delim);
     token = std::strtok(nullptr, delim);
+    std::cout<<token<<std::endl;
     if(token != nullptr)
       respArr.push_back(token);
   }
@@ -59,12 +60,12 @@ void handle_response(int client_fd)
     int byte_rec = recv(client_fd, buffer, sizeof(buffer), 0);
     if(byte_rec <= 0)
       break;
-    std::cout<<buffer;
+
     std::vector<char*> respArr = parserRESP(buffer);
     std::string resp;
     if(respArr.size() == 0)
       resp = buildBulkString(respArr);
-    std::cout<<resp;
+
     const char *response =  resp.c_str();
     send(client_fd, response, strlen(response), 0);
    }
